91abbcaedad5bab4bf297520858dfb84290bee28
Ja-yRo <roper.jay@hotmail.com>
2024-11-11 13:37:56 +0000

    convert to lowercase

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -6,37 +6,32 @@ public class CurrencyConverter {
     public static void main(String[] args) {
 
         double amount, dollar, pound, euro;
-
         DecimalFormat f = new DecimalFormat("##.##");
 
         amount = Integer.parseInt(args[0]);
-        String currency = args[1];
+        String currency = args[1].toLowerCase();
 
         // For amounts Conversion
-        switch (currency.toLowerCase()) {
+        switch (currency) {
             case "dollars":
-                // For Dollar Conversion
                 pound = amount * 0.74;
                 System.out.println(amount + " Dollars = " + f.format(pound) + " Pounds");
                 euro = amount * 0.88;
                 System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
                 break;
             case "pounds":
-                // For Pound Conversion
                 dollar = amount * 1.36;
                 System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
                 euro = amount * 1.19;
                 System.out.println(amount + " Pounds = " + f.format(euro) + " Euros");
                 break;
             case "euros":
-                // For Euro Conversion
                 dollar = amount * 1.13;
                 System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
                 pound = amount * 0.84;
                 System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
                 break;
         }
-	//Change "Thank you" to "Thank's" to test jenkins auto update
-        System.out.println("Thank's for using the converter.");
+        System.out.println("Thank you for using the converter.");
     }
 }
